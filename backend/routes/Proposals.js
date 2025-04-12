5be7319e6ab4d69579fe179038fd17d8e708ee55
KowshiK Mozumder <102278374+Kowshik32@users.noreply.github.com>
2025-04-12 12:20:24 +0600

    submit Proposals feature update.js

--- backend/routes/Proposals.js
+++ backend/routes/Proposals.js
@@ -10,22 +10,7 @@ const {
   getAcceptedProposals
 } = require('../controllers/proposalController');
 
-// Get all proposals
-router.get('/', getProposals);
-
-// Get accepted proposals
-router.get('/accepted', getAcceptedProposals);
-
-// Get single proposal
-router.get('/:id', getProposalById);
 
 // Create new proposal
 router.post('/', createProposal);
-
-// Update proposal
-router.put('/:id', updateProposal);
-
-// Delete proposal
-router.delete('/:id', deleteProposal);
-
 module.exports = router;
