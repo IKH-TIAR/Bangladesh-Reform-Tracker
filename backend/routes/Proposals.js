6e9117ffe783206e0f6b19e8cb9947a58887171d
KowshiK Mozumder <102278374+Kowshik32@users.noreply.github.com>
2025-04-12 20:48:32 +0600

    Impementation complete backend accepted proposal.js

--- backend/routes/Proposals.js
+++ backend/routes/Proposals.js
@@ -1,4 +1,4 @@
-// backend/routes/proposals.js
+
 const express = require('express');
 const router = express.Router();
 const { 
@@ -9,23 +9,4 @@ const {
   deleteProposal,
   getAcceptedProposals
 } = require('../controllers/proposalController');
-
-// Get all proposals
-router.get('/', getProposals);
-
-// Get accepted proposals
 router.get('/accepted', getAcceptedProposals);
-
-// Get single proposal
-router.get('/:id', getProposalById);
-
-// Create new proposal
-router.post('/', createProposal);
-
-// Update proposal
-router.put('/:id', updateProposal);
-
-// Delete proposal
-router.delete('/:id', deleteProposal);
-
-module.exports = router;
